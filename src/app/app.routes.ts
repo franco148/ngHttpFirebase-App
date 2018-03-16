a224f699c13c483b996c3f6b25b6ee09db0fd83f
franco-extreme <franck_ral@hotmail.com>
2018-03-16 16:24:20 -0400

    ngHttpFirebase: Components' styles and forms for edition.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroEditComponent } from './components/heroes/hero-edit.component';
 
 const APP_ROUTES: Routes = [
-  { path: 'horoes', component: HeroesComponent },
-  { path: 'horoe/:id', component: HeroEditComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'horoes' }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'hero/:id', component: HeroEditComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
